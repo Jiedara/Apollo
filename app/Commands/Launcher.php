7049c1320325180671c10bf1ce935f792b5a5213
Jiedara <m.villanove@laposte.net>
2018-10-31 14:43:54 +0100

    change creation of composer.json to composer create-project and edit composer.json afterward

--- app/Commands/Launcher.php
+++ app/Commands/Launcher.php
@@ -23,6 +23,12 @@ class Launcher extends Command
      */
     protected $description = 'Launch a brand new Laravel App';
 
+    public $composerApp;
+
+    public $name;
+    public $directory;
+    public $appPath;
+
     /**
      * Create a new command instance.
      *
@@ -30,6 +36,9 @@ class Launcher extends Command
      */
     public function __construct()
     {
+        $this->composerApp = new ComposerApplication();
+        $this->composerApp->setAutoExit(false); // prevent `$application->run` method from exitting the script
+
         parent::__construct();
     }
 
@@ -46,92 +55,110 @@ class Launcher extends Command
         $this->line('');
         $this->line('');
 
-        $name = $this->ask('What is the app name ?');
-
-        $directory = $this->ask('Where do you want to create the "' . $name . '" app ?');
-
-        $appPath = $directory . DIRECTORY_SEPARATOR . $name;
-
-        if (!is_dir($appPath)) {
-            $this->info('Creating ' . $appPath);
-            mkdir($appPath, 0755, true);
-        } else {
-            $this->error('The directory ' . $appPath . ' already exist');
-            die;
-        }
-
-        $this->info('Creating Simple Laravel App in ' . $appPath);
-
-        if (!$this->createComposerJson($name, $appPath)) {
-            $this->error('Could not create composer.json file in ' . $appPath);
-            die;
-        }
+        $this->name = "plopiplop";
+        $this->directory = "/home/jiedara/Code";
+        $this->appPath = "/home/jiedara/Code/plopiplop";
 
         // Composer\Factory::getHomeDir() method
         // needs COMPOSER_HOME environment variable set
         putenv('COMPOSER_HOME=' . getcwd() . '/vendor/bin/composer');
 
-        chdir($appPath);
+        chdir($this->appPath);
 
-        $composerApp = new ComposerApplication();
-        $composerApp->setAutoExit(false); // prevent `$application->run` method from exitting the script
+        $this->updateComposerJson();
+        die;
 
-        // call `composer create project` command programmatically
-        $input = new ArrayInput(array('command' => 'install'));
-        $composerApp->run($input);
+        $this->name = $this->ask('What is the app name ?');
 
-        $this->line("Done.");
+        $this->directory = $this->ask('Where do you want to create the "' . $this->name . '" app ?');
+
+        $this->appPath = $this->directory . DIRECTORY_SEPARATOR . $this->name;
+
+        if (!is_dir($this->appPath)) {
+            $this->info('Creating ' . $this->appPath);
+            mkdir($this->appPath, 0755, true);
+        } else {
+            $this->error('The directory ' . $this->appPath . ' already exist');
+            die;
+        }
+
+        $this->info('Creating Simple Laravel App in ' . $this->appPath);
+
+        // Composer\Factory::getHomeDir() method
+        // needs COMPOSER_HOME environment variable set
+        putenv('COMPOSER_HOME=' . getcwd() . '/vendor/bin/composer');
+
+        chdir($this->appPath);
+
+        $this->createLaravelApp();
+
+        $this->line("");
+        $this->info("Basic Laravel application created.");
+
+        $this->line("");
+        $this->line("Now, let's personalize it !");
+
+        $this->updateComposerJson();
+
     }
 
-    protected function createComposerJson($name, $appPath)
+    public function createLaravelApp()
     {
-        $composerJson = [
-            "name" => $name,
-            "description" => "Project created with Apollo.",
-            "keywords" => ["laravel", "apollo"],
-            "type" => "project",
-            "require" => [
-                "php" => "^7.1.3",
-                "laravel/framework" => "5.6.*",
-                "laravel/tinker" => "1.0.*",
-                "laravelcollective/html" => "5.6.*",
-            ],
-            "require-dev" => [
-                "barryvdh/laravel-debugbar" => "3.2.*",
-                "barryvdh/laravel-ide-helper" => "2.5.*",
-                "fzaninotto/faker" => "1.8.*",
-                "laravel/dusk" => "3.0.*",
-                "mockery/mockery" => "1.1.*",
-                "phpunit/phpunit" => "7.3.*"
-            ],
-            "autoload" => [
-                "psr-4" => [
-                    "App\\" => "app/"
-                ]
-            ],
-            "autoload-dev" => [
-                "psr-4" => [
-                    "Tests\\" => "tests/"
-                ]
-            ],
-            "minimum-stability" => "dev",
-            "prefer-stable" => true
-        ];
+        // call `composer create project` command programmatically
+        $input = new ArrayInput(
+            array(
+                'command' => 'create-project',
+                'package' => 'laravel/laravel',
+                'directory' => $this->appPath
+            )
+        );
+        $this->composerApp->run($input);
+    }
 
-        $composerJson['description'] = $this->ask('Description of the project : ["Project created with Apollo"]') ?? "Project created with Apollo";
+    protected function updateComposerJson($composerJson = null)
+    {
+        if (null === $composerJson) {
+            $composerJson = json_decode(file_get_contents($this->appPath . DIRECTORY_SEPARATOR . 'composer.json'), true);
+        } else {
+            file_put_contents($this->appPath . DIRECTORY_SEPARATOR . 'composer.json', json_encode($composerJson, JSON_UNESCAPED_SLASHES + JSON_PRETTY_PRINT));
+        }
+
+        $backupComposerJson = $composerJson;
+
+        $composerJson['description'] = $this->ask('Description of the project ["Project created with Apollo"]') ?? "Project created with Apollo";
 
         $package = "";
         while (null !== $package) {
-            $package = $this->ask('Add a specific package to the project (require) : [empty to skip]');
-            $composerJson['require'][] = $package;
+            $package = $this->ask('Add a specific package to the project (require) [empty to skip]');
+            if (null !== $package) {
+                $packageVersion = $this->ask('What version to use for the package ' . $package . ' ? [*]') ?? "*";
+                $composerJson['require'][$package] = $packageVersion;
+            }
         }
 
         $devPackage = "";
         while (null !== $devPackage) {
-            $devPackage = $this->ask('Add a specific dev package to the project (require-dev) : [empty to skip]');
-            $composerJson['require-dev'][] = $devPackage;
+            $devPackage = $this->ask('Add a specific dev package to the project (require-dev) [empty to skip]');
+            if (null !== $devPackage) {
+                $packageVersion = $this->ask('What version to use for the package ' . $package . ' ? [*]') ?? "*";
+                $composerJson['require-dev'][$devPackage] = $packageVersion;
+            }
         }
 
-        return file_put_contents($appPath . DIRECTORY_SEPARATOR . 'composer.json', json_encode($composerJson, true));
+        file_put_contents($this->appPath . DIRECTORY_SEPARATOR . 'composer.json', json_encode($composerJson, JSON_UNESCAPED_SLASHES + JSON_PRETTY_PRINT));
+
+        if ($composerJson['require'] !== $backupComposerJson['require'] || $composerJson['require-dev'] !== $backupComposerJson['require-dev'] ) {
+            // call `composer update` command programmatically
+            $input = new ArrayInput(
+                array(
+                    'command' => 'update',
+                )
+            );
+            //composerApp return 2 when there's an error with the packages
+            if($this->composerApp->run($input) == 2) {
+                $this->error("Some package are not valid. Let's try again !");
+                $this->updateComposerJson($backupComposerJson);
+            }
+        }
     }
 }
